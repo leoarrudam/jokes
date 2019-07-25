83069a8e55a20bd6e0f4a51e99bb592386de1167
Danillo Rodrigues de Almeida <danillo_jp11@hotmail.com>
2019-07-25 15:42:54 -0300

    projeto inicial

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { JokesService } from '../services/jokes.service';
+
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,44 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private jokesService: JokesService) {}
+  valorInput: string;
+  piada: any[];  
+  piadas: any[];
+
+  ngOnInit(){
+    this.jokesService.getAllJokes().subscribe(piadas=>{
+      this.piadas = piadas;
+ 
+     });
+  }
+
+
+
+  showJoke(){
+    this.jokesService.getAllJokes().subscribe(piadas=>{
+
+     this.searchOnJokes(); 
+
+    }, erro=>{
+      console.log(erro);
+    }, ()=>{
+      console.log('sucesso');
+    });
+  }
+
+  indexAleatorio(piadas){
+    const index=  Math.floor(Math.random()* piadas.length + 1);
+    return index;
+  }
+
+  searchOnJokes() {
+    this.piadas = this.piadas.filter(elemento=>{
+      if (elemento.pergunta.search(this.valorInput)){
+        return elemento;
+      }
+
+    })
+  }
 
 }
